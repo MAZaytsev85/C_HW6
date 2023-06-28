6c5b569146d15ef1a4da1520352702e689580a67
MAZaytsev85 <streetrsr2007@yandex.ru>
2023-06-28 19:43:19 +0300

    Домашняя работа №6

---
+++ Task_41/Program.cs
@@ -0,0 +1,48 @@
+﻿// Пользователь вводит с клавиатуры M
+// чисел. Посчитайте, сколько чисел больше 0 ввёл
+// пользователь.
+
+int m = Prompt("Введите количество элементов");
+int[] array = GetArr(m);
+PrintArr(array);
+Console.WriteLine($"-> {CountArrayElement(array)}");
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int[] GetArr(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt("Введите число");
+    }
+    return array;
+}
+
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int CountArrayElement(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
---
+++ Task_43/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая найдёт
+// точку пересечения двух прямых, заданных
+// уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+double k1 = Prompt("Введите значение k1");
+double b1 = Prompt("Введите значение b1");
+double k2 = Prompt("Введите значение k2");
+double b2 = Prompt("Введите значение b2");
+
+Result(k1, b1, k2, b2);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void Result(double k1, double b1, double k2, double b2)
+{
+
+    if ((k1 * b2 - k2 * b1) == 0)
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else if (k1 * b2 == b1 * k2)
+    {
+        Console.WriteLine("Прямые совпадают");
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine("Точка пересечения прямых: ({0}, {1})", x, y);
+    }
+}
+
